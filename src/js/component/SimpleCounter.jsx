e444c11ae2770e1debc55b81c8cbd7fac88ca20e
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-27 00:51:15 +0000

    up1

---
+++ src/js/component/SimpleCounter.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SimpleCounter = (props) => {
+
+    return (
+
+<div className="counter-display">
+    <div className="clock-img"><svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" fill="currentColor" className="bi bi-stopwatch" viewBox="0 0 16 17">
+  <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5z"/>
+  <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64l.012-.013.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3"/>
+</svg></div>
+    <div className="seconds hundred-thousands">0</div>
+    <div className="seconds ten-thousands">0</div>
+    <div className="seconds thousands">0</div>
+    <div className="seconsd hundres">0</div>
+    <div className="seconds tens">0</div>
+    <div className="seconds ones">{Props.onesDigit.counter}</div>
+<div/>
+<div/>
+</div>
+    )
+}
+
+export default SimpleCounter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,25 @@
 import React from "react";
+import SimpleCounter from "./SimpleCounter";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+function calculateSeconds(aCounter, placeValue) {
+return Math.floor(aCounter / placeValue) % 10
+
+}
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		<SimpleCounter 
+		hundred-thousandsDigit= {calculateSeconds(props.counter, 100000)}
+		ten-thousandsDigit= {calculateSeconds(props.counter, 10000)}
+		thousandsDigit= {calculateSeconds(props.counter, 1000)}
+		hundresDigit= {calculateSeconds(props.counter, 100)}
+		tensDigit= {calculateSeconds(props.counter, 10)}
+		onesDigit= {calculateSeconds(props.counter, 1)}
+		/>
+		</>
 	);
 };
 
