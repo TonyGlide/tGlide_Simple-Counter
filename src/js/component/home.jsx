450773daa08de294622e3fe51be03c42746e6a58
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-27 01:08:12 +0000

    f_1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,12 +12,12 @@ const Home = (props) => {
 	return (
 		<>
 		<SimpleCounter 
-		hundred-thousandsDigit= {calculateSeconds(props.counter, 100000)}
-		ten-thousandsDigit= {calculateSeconds(props.counter, 10000)}
-		thousandsDigit= {calculateSeconds(props.counter, 1000)}
-		hundresDigit= {calculateSeconds(props.counter, 100)}
-		tensDigit= {calculateSeconds(props.counter, 10)}
-		onesDigit= {calculateSeconds(props.counter, 1)}
+		Digit6= {calculateSeconds(props.counter, 100000)}
+		Digit5= {calculateSeconds(props.counter, 10000)}
+		Digit4= {calculateSeconds(props.counter, 1000)}
+		Digit3= {calculateSeconds(props.counter, 100)}
+		Digit2= {calculateSeconds(props.counter, 10)}
+		Digit1= {calculateSeconds(props.counter, 1)}
 		/>
 		</>
 	);
