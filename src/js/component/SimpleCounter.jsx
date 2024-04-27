677186de1ad20b17c115d7b0d9503c7b6fab64c5
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-27 00:58:08 +0000

    up2

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -9,12 +9,12 @@ const SimpleCounter = (props) => {
   <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5z"/>
   <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64l.012-.013.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3"/>
 </svg></div>
-    <div className="seconds hundred-thousands">0</div>
-    <div className="seconds ten-thousands">0</div>
-    <div className="seconds thousands">0</div>
-    <div className="seconsd hundres">0</div>
-    <div className="seconds tens">0</div>
-    <div className="seconds ones">{Props.onesDigit.counter}</div>
+    <div className="seconds hundred-thousands">{props.hundred-thousandsDigit}</div>
+    <div className="seconds ten-thousands">{props.ten-thousandsDigit}</div>
+    <div className="seconds thousands">{props.thousandsDigit}</div>
+    <div className="seconsd hundres">{props.hundresDigit}</div>
+    <div className="seconds tens">{props.tensDigit}</div>
+    <div className="seconds ones">{props.onesDigit}</div>
 <div/>
 <div/>
 </div>
